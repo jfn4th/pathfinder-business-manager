f6e8f88e6891ec17e52b4e6eeec6d9307c5bd04e
Jamie Nelson <jfn4th@gmail.com>
2020-07-20 19:44:49 -0500

    add node index

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+    // businessRouter = require('./routes/businessRouter'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    // db = require('./db'),
+    app = express(),
+    apiPort = 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// db.on('error', console.error.bind(console, 'MongoDB Atlas connection error:'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// app.use('/api', businessRouter);
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
