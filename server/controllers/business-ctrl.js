bf39f82e468bca94e36c850ddc709df078cdaaaa
Jamie Nelson <jfn4th@gmail.com>
2020-07-22 22:15:49 -0500

    add controller and route files, controller structure

---
+++ server/controllers/business-ctrl.js
@@ -0,0 +1,15 @@
+const Business = require('../models/business');
+
+createBusiness = (req, res) => {};
+getBusinessesByCharacter = (req, res) => {};
+getBusinessById = (req, res) => {};
+updateBusiness = (req, res) => {};
+deleteBusiness = (req, res) => {};
+
+module.exports = {
+    createBusiness,
+    getBusinessesByCharacter,
+    getBusinessesByCharacter,
+    updateBusiness,
+    deleteBusiness
+};
---
+++ server/controllers/character-ctrl.js
@@ -0,0 +1,15 @@
+const Character = require('../models/Character');
+
+createCharacter = (req, res) => {};
+getCharacters = (req, res) => {};
+getCharacterById = (req, res) => {};
+updateCharacter = (req, res) => {};
+deleteCharacter = (req, res) => {};
+
+module.exports = {
+    createCharacter,
+    getCharacters,
+    getCharactereById,
+    updateCharacter,
+    deleteCharacter
+};
