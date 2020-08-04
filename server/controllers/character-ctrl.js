475e928fdac0f36a53ab48f976f038109ec26c02
Jamie Nelson <jfn4th@gmail.com>
2020-08-04 12:57:04 -0500

    write create character controller

--- server/controllers/character-ctrl.js
+++ server/controllers/character-ctrl.js
@@ -1,6 +1,37 @@
 const Character = require('../models/Character');
 
-createCharacter = (req, res) => {};
+createCharacter = (req, res) => {
+    const body = req.body;
+
+    if (!body) {
+        return RandomSource.status(400).json({
+            success: false,
+            error: 'You must provide a character!'
+        });
+    }
+
+    const character = new Character(body);
+
+    if (!character) {
+        return res.status(400).json({ success: false, error: err });
+    }
+
+    character
+        .save()
+        .then(() => {
+            return res.status(201).json({
+                success: true,
+                id: character._id,
+                message: 'Character created!'
+            });
+        })
+        .catch((error) => {
+            return res.status(400).json({
+                error,
+                message: 'Character not created!'
+            });
+        });
+};
 getCharacters = (req, res) => {};
 getCharacterById = (req, res) => {};
 updateCharacter = (req, res) => {};
